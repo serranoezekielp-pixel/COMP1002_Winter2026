90fda92a320b1698122f08b3974c5392ba13047d
serranoezekielp-pixel <serranoezekielp@gmail.com>
2026-04-02 23:49:57 -0400

    Completed Ass.3

--- assessments/assignment_three/broken.js
+++ assessments/assignment_three/broken.js
@@ -4,7 +4,7 @@
 let helloBtn = document.querySelector("#helloBtn");
 let greetingOutput = document.querySelector("#greetingOutput");
 
-let themeBtn = document.querySelector("#themeButton");
+let themeBtn = document.querySelector("#themeBtn"); //Changed variable name from "themeToggle" to "themeBtn" to match the HTML
 
 let messageInput = document.querySelector("#messageInput");
 let liveOutput = document.querySelector("#liveOutput");
@@ -20,16 +20,16 @@ helloBtn.addEventListener("click", function() {
 
 // 2. Theme Toggle
 themeBtn.addEventListener("click", function() {
-    document.body.classList.toggle("darkmode");
+    document.body.classList.toggle("dark-mode"); //Changed "darkMode" to "dark-mode" to match the CSS class name
 });
 
 // 3. Live Input
-messageInput.addEventListener("change", function() {
-    liveOutput.textContent = messageinput.value;
+messageInput.addEventListener("input", function() { //"input" event should be used instead of "change" to update live as the user types    
+    liveOutput.textContent = messageInput.value; //"messageInput" should be capitalized to match the variable name
 });
 
 // 4. Welcome Form
-welcomeForm.addEventListener("submit", function() {
+welcomeForm.addEventListener("submit", function(event) { //added "event" parameter to the function to use in preventDefault
     event.preventDefault();
     formOutput.textContent = "Welcome, " + nameInput.value;
 });
